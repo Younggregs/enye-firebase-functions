eb32b562942aa3fc00761789068aaf05367143a6
Retzam Tarle <Younggregs>
2019-10-28 16:22:35 +0100

    first commit

---
+++ functions/index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+// Firebase init
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Express and CORS middleware init
+const app = express();
+app.use(cors());
+
+// POST / method
+app.post("/", (request, response) => {
+    const entry = request.body;
+
+    return admin.database().ref('/entries').push(entry)
+        .then(() => {
+            return response.status(200).send(entry)
+        }).catch(error => {
+            console.error(error);
+            return response.status(500).send('Error: ' + error);
+        });
+});
+
+
+
+// GET / method
+app.get("/", (request, response) => {
+    return admin.database().ref('/entries').on("value", snapshot => {
+        return response.status(200).send(snapshot.val());
+    }, error => {
+        console.error(error);
+        return response.status(500).send(' Error: ' + error);
+    });
+});
+
+exports.entries = functions.https.onRequest(app);
